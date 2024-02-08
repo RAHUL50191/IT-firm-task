2921a0dbd1b9ed40a64bb119460bb9433b38ff54
Rahul <rahul200283@gmail.com>
2024-02-08 11:29:27 +0530

    client env

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,4 @@
-exports.CLIENT = "http://127.0.0.1:3000";
-exports.SERVER = "http://127.0.0.1:3001";
+// exports.CLIENT = "http://127.0.0.1:3000";
+// exports.SERVER = "http://127.0.0.1:3001";
+exports.CLIENT = "https://it-firm-task.vercel.app/";
+exports.SERVER = "https://it-firm-task-cl73.vercel.app/";
