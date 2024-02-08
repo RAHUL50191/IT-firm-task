83e1606a68ca4aa5cc6a854e31d43f61782cbe18
Rahul <rahul200283@gmail.com>
2024-02-08 11:11:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
 app.use(express.json());
-
+app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 app.use(
   cors({
@@ -26,6 +26,12 @@ mongoose
   .catch((error) => console.error("Failed to connect to MongoDB", error));
 app.get("/", (req, res) => {
   res.send({ msg: "hello from server" });
+});
+app.get("/api", (req, res) => {
+  const path = `/api`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${process.env.CLIENT}">client</a>`);
 });
 app.use("/user", userRouter);
 app.listen(PORT, () => {
